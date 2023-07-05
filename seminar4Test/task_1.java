369b36a5873f52791579f9818866f0d766f655d5
Sergei <sandeeey@example.com>
2023-07-05 16:13:41 +0300

    java_1

---
+++ seminar4Test/task_1.java
@@ -0,0 +1,25 @@
+package seminar4Test;
+
+import java.util.LinkedList;
+public class task_1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(getRandomNumber());
+        }
+        System.out.println(ll);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(ll.getLast() + ", ");
+            ll.removeLast();
+
+        }
+    }
+
+    public static int getRandomNumber() { // генерирует случайное число
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
+}
+}
